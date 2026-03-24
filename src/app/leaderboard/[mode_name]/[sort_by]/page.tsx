bf2dbcb759d8dd14ca7097a838e33f5d6f1778d3
basshhii0610 <kachakun0610@gmail.com>
2026-03-24 16:57:20 +0900

    commit files

--- src/app/leaderboard/[mode_name]/[sort_by]/page.tsx
+++ src/app/leaderboard/[mode_name]/[sort_by]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation";
-import { SortBy, getCountryList, getLeaderboard } from "@/database/leaderboard";
-import { Mode, ModeNum } from "@/lib/mode";
+import { SortBy, getCountryList } from "@/database/leaderboard";
+import { Mode } from "@/lib/mode";
 import { writeLog } from "@/lib/log";
 import RankingList from "@/components/leaderboard/ranking-list";
 import styles from "@s/leaderboard.module.css";
@@ -23,7 +23,7 @@ export default async function Leaderboard({ params, searchParams }: {
 	const conds = [
 		Object.values(Mode).includes(mode_name as Mode),
 		Object.values(SortBy).includes(sort_by as SortBy),
-		Number(page) > 0,
+		!isNaN(Number(page)) && Number(page) > 0,
 		(country === undefined && clan === undefined) || (country === undefined && clan !== undefined) || (country !== undefined && clan === undefined),
 		country === undefined || countries.find((value) => value.country === country) !== undefined,
 		clan === undefined || clan === ""
@@ -33,13 +33,14 @@ export default async function Leaderboard({ params, searchParams }: {
 	
 	if (conds.every((cond) => cond)) {
 		const mode = mode_name as Mode, sortBy = sort_by as SortBy, isClan = clan !== undefined;
-		const { ranking, pages } = await getLeaderboard(ModeNum[mode], sortBy, Number(page), isClan, country);
 		
 		return (
-			<div className={styles.leaderboard_wrapper}>
-				<Suspense fallback={null/*TODO*/}>
-					<RankingList ranking={ranking} mode={mode} sortBy={sortBy} isClan={isClan} />
-				</Suspense>
+			<div className={styles.leaderboard_container}>
+				<div className={styles.table_wrapper}>
+					<Suspense fallback={null/*TODO*/}>
+						<RankingList mode={mode} sortBy={sortBy} page={Number(page)} isClan={isClan} country={country} />
+					</Suspense>
+				</div>
 			</div>
 		);
 	}
--- src/components/leaderboard/ranking-list.tsx
+++ src/components/leaderboard/ranking-list.tsx
@@ -1,15 +1,18 @@
-import type { RankingList } from "@/database/leaderboard";
+import { getLeaderboard } from "@/database/leaderboard";
 import { SortBy } from "@/database/leaderboard";
-import { Mode } from "@/lib/mode";
+import { Mode, ModeNum } from "@/lib/mode";
 import RankingHeader from "@/components/leaderboard/ranking-header";
 import RankingRow from "@/components/leaderboard/ranking-row";
 
-export default function RankingList({ ranking, mode, sortBy, isClan }: Readonly<{
-	ranking: RankingList[],
+export default async function RankingList({ mode, sortBy, page, isClan, country }: Readonly<{
 	mode: Mode,
 	sortBy: SortBy,
-	isClan: boolean
+	page: number,
+	isClan: boolean,
+	country: string | undefined
 }>) {
+	const { ranking, pages } = await getLeaderboard(ModeNum[mode], sortBy, page, isClan, country);
+	
 	return (
 		<>
 			{ranking.length > 0 &&
