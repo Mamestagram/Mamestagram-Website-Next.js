d550348b97ad8d749a3b2e52f162c2f1da865537
basshhii0610 <kachakun0610@gmail.com>
2026-04-01 09:40:16 +0900

    commit files

--- src/components/leaderboard/page-list.tsx
+++ src/components/leaderboard/page-list.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import classNames from "classnames";
 import Link from "next/link";
-import { createRef } from "react";
+import { createRef, useEffect } from "react";
 import { OsuMode } from "@/lib/mode";
 import { SortBy } from "@/database/leaderboard";
 import FontAwesome from "@/components/font-awesome";
@@ -26,6 +27,24 @@ export default function PageList({ page, mode, sortBy, isClan, country }: {
 	const queryStr = queries.length > 0 ? `&${queries.join("&")}` : "";
 	const pageRefs = Array.from({ length: 5 }, () => createRef<HTMLLIElement>());
 	
+	const clickChevron = (e: MouseEvent) => {
+		const element = e.target as HTMLButtonElement;
+		const prevRefs = [...pageRefs];
+		prevRefs.forEach((ref, i) => {
+			if (element.classList.contains("left") && i > 0) {
+			
+			}
+			else if (element.classList.contains("right") && i > 0) {
+			
+			}
+		});
+	}
+	
+	useEffect(() => {
+		document.querySelector(`.${styles.page_wrapper} .${styles.page_list} li`)?.classList.remove("show");
+		pageRefs.forEach((ref) => { ref.current?.classList.add("show"); });
+	}, [pageRefs]);
+	
 	return (
 		<div className={styles.page_wrapper}>
 			{page.current > 1 &&
@@ -33,15 +52,17 @@ export default function PageList({ page, mode, sortBy, isClan, country }: {
 					<FontAwesome prefix="fas" name="chevrons-left"/>
 				</Link>
 			}
-			<button><FontAwesome prefix="fas" name="chevron-left"/></button>
+			<button className="shift left"><FontAwesome prefix="fas" name="chevron-left"/></button>
 			<ul className={styles.page_list}>
 				{Array.from({ length: page.total }).map((_val, i) =>
-					<li key={i} ref={}>
+					<li key={i}
+					    className={classNames(`page-${i + 1}`, { [styles.current_page]: i + 1 === page.current })}
+					    ref={Math.abs(i + 1 - page.current) <= 2 ? pageRefs[i - page.current + 3] : undefined}>
 						<Link href={`/leaderboard/${mode}/${sortBy}?page=${i + 1}${queryStr}`}>{i + 1}</Link>
 					</li>
 				)}
 			</ul>
-			<button><FontAwesome prefix="fas" name="chevron-right"/></button>
+			<button className="shift right"><FontAwesome prefix="fas" name="chevron-right"/></button>
 			{page.current < page.total &&
 				<Link href={`/leaderboard/${mode}/${sortBy}?page=${page.current + 1}${queryStr}`}>
 					<FontAwesome prefix="fas" name="chevrons-right"/>
