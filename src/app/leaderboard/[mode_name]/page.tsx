b1ee7418ac829b50bf0ba4207a990f93a7525905
basshhii0610 <kachakun0610@gmail.com>
2026-04-21 12:32:40 +0900

    リーダーボード自動リダイレクト修正

--- src/app/leaderboard/[mode_name]/page.tsx
+++ src/app/leaderboard/[mode_name]/page.tsx
@@ -4,7 +4,7 @@ import { OsuMode } from "@/lib/mode";
 export default async function ModeName({ params }: { params: Promise<{ mode_name: string }> }) {
 	const { mode_name } = await params;
 	if (Object.values(OsuMode).includes(mode_name as OsuMode))
-		redirect(`/leaderboard/${mode_name}/performance`);
+		redirect(`${mode_name}/performance`);
 	else
 		notFound();
 }
--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function NoParams() {
-	redirect("/leaderboard/std/performance");
+	redirect("leaderboard/std/performance");
 }
