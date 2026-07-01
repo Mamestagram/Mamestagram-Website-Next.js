6f5758586f74bc510023c8e478c114bc7a0a9670
basshhii0610 <kachakun0610@gmail.com>
2026-07-01 21:26:15 +0900

    めんどいからやめた

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
 		remotePatterns: [
 			{ protocol: "https", hostname: `a.${process.env.BASE_DOMAIN}` },
 			{ protocol: "https", hostname: `clan-a.${process.env.BASE_DOMAIN}` },
+			{ protocol: "https", hostname: `assets.${process.env.BASE_DOMAIN}` },
 			{ protocol: "https", hostname: "assets.ppy.sh" },
 		]
 	}
